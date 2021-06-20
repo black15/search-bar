10bc24cc3e94009a3ef0cd6cbcf90e210ad6a3b7
~#/usr/bin/b14ck <38563357+black15@users.noreply.github.com>
2021-06-20 21:47:15 +0100

    Create script.js

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+let 
+    icon    = document.querySelector('.icon'),
+    input   = document.querySelector('.input'),
+    search  = document.querySelector('.search');
+
+function clicked(){
+    search.classList.toggle('active')
+}
+function clearIt(){
+    document.getElementById('clearMe').value ='';
+}
